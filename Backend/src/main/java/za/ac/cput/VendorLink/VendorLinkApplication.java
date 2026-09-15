8f49603be84355885668aab1ba17422ed1641899
M-WazeerG <wazeergil40@gmail.com>
2026-09-15 21:49:53 +0200

    Add backend .gitignore and organize modules

---
+++ Backend/src/main/java/za/ac/cput/VendorLink/VendorLinkApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.VendorLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorLinkApplication.class, args);
+	}
+
+}
